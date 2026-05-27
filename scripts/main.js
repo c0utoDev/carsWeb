96311d9d2368c3c925d24eef349354cf85c00f46
c0utoDev <rhucoutinhoxd@gmail.com>
2026-05-26 22:55:08 -0300

    criação da página js e adição do botão no arquivo nissan.html

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+
+const comprar = document.getElementById("comprar")
+if (comprar) {
+    comprar.onclick = function comprar() {
+        alert("Comprado!");
+    }
+} else {
+    console.error("Não foi possível encontrar o id!");
+
+}
